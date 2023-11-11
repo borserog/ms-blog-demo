2684e5eb7ac7aaf553dd077e4eb6ac526cd725ce
Gabriel Borsero <gabriel.borsero@gmail.com>
2023-11-10 22:15:11 -0300

    event-bus

---
+++ event-bus/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const getMsUrl = (num) => `http://localhost:400${num}/events`;
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    axios.post(getMsUrl(0), event);
+    axios.post(getMsUrl(1), event);
+    axios.post(getMsUrl(2), event);
+
+    res.send({status: 'OK'});
+});
+
+
+app.listen(4005, () => {
+    console.log('Listening on 4005');
+})
